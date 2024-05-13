b9368b21481045b26c5ef5557f60ffe7fccee523
Mshibe700 <philasandeluthuli77@gmail.com>
2024-05-13 09:29:11 -0700

    Task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/Mshibe700/alx-low_level_programming/master/0x18-dynamic_libraries/svrhack.so
-export LD_PRELOAD="$PWD/../svrhack.so"
+wget -P /tmp https://github.com/Mshibe700/alx-low_level_programming/master/0x18-dynamic_libraries/svrhack.so
+export LD_PRELOAD=/tmp/svrhack.so
